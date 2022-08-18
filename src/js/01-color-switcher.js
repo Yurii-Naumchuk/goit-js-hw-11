3b4ca483ceef8555ac0e1d06ae8a1700c62888e1
Yurii-Naumchuk <uranaumchuk7@gmail.com>
2022-08-18 18:42:58 +0300

    1 task finalli done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,34 +1,50 @@
-// const startBtn = document.querySelector('button[data-start]');
-// const stopBtn = document.querySelector('button[data-stop]');
-// const bodyColor =document.querySelector('body');
+// Напиши скрипт, який після натискання кнопки «Start»,
+//  раз на секунду змінює колір фону <body> на випадкове значення, 
+//  використовуючи інлайн стиль. Натисканням на кнопку «Stop» 
+//  зміна кольору фону повинна зупинятися.
+//  Враховуй, що на кнопку «Start» можна натиснути нескінченну кількість разів. 
+//  Зроби так, щоб доки зміна теми запущена, кнопка «Start» була неактивною (disabled).
+//  Для генерування випадкового кольору використовуй функцію getRandomHexColor.
 
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const bodyColor =document.querySelector('body');
 
 stopBtn.disabled = true;
+startBtn.style.background ='#50db2a';
 
 startBtn.addEventListener('click',startBtnClick)
 stopBtn.addEventListener('click',stopBtnClick)
 
-const changeBackColor=()=>bodyColor.style.backgroundColor = getRandomHexColor();
+const changeColor=()=>bodyColor.style.backgroundColor = getRandomHexColor();
 
-let intarvalId= null;
+let intarvalId;
 
 function startBtnClick(){
-    intarvalId=setInterval(changeBackColor, 1000)
+    intarvalId=setInterval(changeColor, 1000)
 
-    startBtnClick.disabled= true;
-    stopBtnClick.disabled= false;
+    startBtn.disabled= true;
+    stopBtn.disabled= false;
+
+    stopBtn.style.background = '#de632f';
+    startBtn.style.background = '#eaf0e9';
+
+    // stopBtn.style.background = '#de632f';
+    // startBtn.style.background = '#50db2a';
 };
 
 function stopBtnClick() {
     clearInterval(intarvalId);
 
-    startBtnClick.disabled= false;
-    stopBtnClick.disabled= true;
+    startBtn.disabled= false;
+    stopBtn.disabled= true;
+
+    startBtn.style.background = '#50db2a';
+    stopBtn.style.background = '#eaf0e9';
+   
 }
 
-
-// function getRandomHexColor() {
-//     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-//   };
-
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  };
 
